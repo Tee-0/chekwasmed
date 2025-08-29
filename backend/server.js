8367992f7362bc4f841ea3c158cc06d7304aec5f
Tee-0 <thelmachekwasnkpa@yahoo.com>
2025-08-29 07:33:52 +0100

    created server file

---
+++ backend/server.js
@@ -0,0 +1,47 @@
+//importing required packages
+const express = require('express');
+const cors = require('cors');
+require(dotenv).config();
+
+//Express app
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//Middleware
+app.use(cors()); // Alllows frontend to communicate with backend
+app.use(express.json()); //Allows server to read JSON data
+
+//Routes
+app.get('/', (req, res)  => {
+    res.json ({
+        message: 'Welcome to ChekwasMed API!',
+        status: 'Server is running successfully',
+        timestamp: new Date().toISOString()
+    });
+});
+
+//Test endpoint for medication conflicts
+app.get('/api/test', (req, res) => {
+    res.json ({
+        message: 'ChekwasMed API test endpoint working!',
+        version: '1.0.0'
+    });
+});
+
+//Basic medication endpoint
+app.post('/api/check-conflicts', (req, res) => {
+    const{medications} = req.body;
+
+    //return medications received
+    res.json ({
+        message: 'Conflict check endpoint working',
+        medications_received: medications || [],
+        conflicts_found: 0
+    });
+});
+
+//Starting the server
+app.listen(PORT, () =>  {
+   console.log('Chekwasmed server is running on port ${PORT}');
+   console.log('Visit: http://localhost:${PORT}');
+});
