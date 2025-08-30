a5d5c62c0ddf4a75b624cdc4637dda5d6ee21021
Tee-0 <thelmachekwasnkpa@yahoo.com>
2025-08-30 09:32:34 +0100

    Basic Server with MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 //importing required packages
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
-require(dotenv).config();
+require('dotenv').config();
 
 //Express app
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+//MongoDB Connection
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.error(' MongoDB connection error:', err));
 
 //Middleware
 app.use(cors()); // Alllows frontend to communicate with backend
